c0fd097476d597b43bea83dd24a81f373c9166bc
MacindiCindi12 <sekhwarimasindi21@gmail.com>
2022-02-03 00:36:05 -0800

    Optional Assessment

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *Main - Entry point
+ *
+ *Return : Always 0 (Success)
+ *
+ */
+
+int main (void)
+{
+       write(2, "and that pece of art is useful\" -Dora Kopar, 2015-10-19\n" , 61);
+
+
+	Return (0);
+}
