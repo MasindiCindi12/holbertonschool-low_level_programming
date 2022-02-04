66dd44aa6d59bc8b66f27aadfe1423b69acf8a9a
Masindi Cindy <59694595+MasindiCindi12@users.noreply.github.com>
2022-02-04 15:28:31 +0200

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,42 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 /**
+ * main - a simple program that outputs 0-9 separated by commas
  *
- * main - Entry point
- *
- *
- * Return; Always 0 (Success)
- *
+ * Return: 0 on success
  */
-
-int main (void)
+int main(void)
 {
-     int d, p, q;
-     for(d='0'; d< '9'; d++)
-     {
-	     for(p=d+1; p<='9'; q++)
-	     {
-		     for(q=p+1;q <= '9' ; p ++)
-		     {
-			     if((p !=d) && P=='8')
-			     {  continue;
+	int i;
+	int j;
+	int k;
 
-			     putchar(',');
-			     putchar(',');
-			     }
-		     }
-	     }
-     }
-}
-
-
-
-
-
-
-
-
+	for (i = 48; i < 56; i++)
+	{
+		for (j = i + 1; j < 57; j++)
+		{
+			for (k = j + 1; k < 58; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != 55 || j != 56 || k != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
 	return (0);
 }
