36754f229eec85044e0c1ade6fa8a312857b213e
Masindi Cindy <59694595+MasindiCindi12@users.noreply.github.com>
2022-02-04 15:47:31 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+
 /**
- * main - unordered combinations of two digit integers without duplicates
+ * main - a simple program that outputs unordered
+ * combinations of two digit integers without duplicates
  *
  * Return: 0 on success
  */
@@ -11,25 +13,25 @@ int main(void)
 	int jfirst;
 	int j;
 
-	for (ifirst = 48 ; ifirst < 58 ; ifirst++)
+	for (ifirst = 48; ifirst < 58; ifirst++)
 	{
-		for (i = 48 ; i < 58 ; i++)
+		for (i = 48; i < 58; i++)
 		{
 			j = i + 1;
 			jfirst = ifirst;
-			for ( ; jfirst < 58 ; jfirst++)
+			for (; jfirst < 58; jfirst++)
 			{
-				for ( ; j < 58 ; j++)
+				for (; j < 58; j++)
 				{
 					putchar(ifirst);
 					putchar(i);
-					putchar('');
+					putchar(' ');
 					putchar(jfirst);
 					putchar(j);
-					if (ifirst ! = 57 || jfirst ! = 57 || i ! = 56 || j ! = 57)
+					if (ifirst != 57 || jfirst != 57 || i != 56 || j != 57)
 					{
 						putchar(',');
-						putchar('');
+						putchar(' ');
 					}
 				}
 				j = 48;
