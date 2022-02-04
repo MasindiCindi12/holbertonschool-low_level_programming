bb9e90a4de1ba5aa9a5fb4191ab3053bcf299612
Masindi Cindy <59694595+MasindiCindi12@users.noreply.github.com>
2022-02-04 15:21:02 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,33 +7,24 @@
  * Return: Always 0 (Success)
  *
  */
-
 int main(void)
 {
       int p, q;
-
        for(p=0;p <=98 ; p++)
        {
-
 	       for (q=p +1; q <= 99 ; q ++)
 	       {
-
-		       putchar((p /10 ) + '0');
+		      putchar((p /10 ) + '0');
 		       putchar((p% 10 ) + '0');
 		       putchar(' ');
 		       putchar((q /10) + '0');
 		       putchar ((q %10) + '0');
 		       if(p ==98 && q==99)
 			       continue;
-
 		       putchar(',');
 		       putchar(',');
-
 	       }
        }
-    
        putchar(',');
-
 	return (0);
-
 }
