55010c20f02cc10b1de6ed5c34bd1b922b40ff1a
Masindi Cindy <59694595+MasindiCindi12@users.noreply.github.com>
2022-02-04 14:28:33 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,21 +8,26 @@
  */
 int main(void)
 {
-	int d;
+	int d, p;
 
-	for (d = 0; d < 90; d++)
+	for (d = '0'; d < '9'; d++)
 	{
-		for ()
+		for (p = d + 1; p <= '9'; p++)
 		{
-			putchar((d / 10) + '0');
-			putchar((d % 10) + '0');
-		}
-		if (d != 89)
-		{
-			putchar(',');
-			putchar(' ');
+			if (p != d)
+			{
+				putchar(d);
+				putchar(p);
+
+				if (d == '8' && p == '9')
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
+
 	return (0);
 }
