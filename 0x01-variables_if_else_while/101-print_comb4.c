3862331294807f1d7bc00b54d69a6d0d34762c40
MacindiCindi12 <sekhwarimasindi21@gmail.com>
2022-02-04 04:53:37 -0800

    Corrections

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ *
+ * Return; Always 0 (Success)
+ *
+ */
+
+int main (void)
+{
+     int d, p, q;
+     for(d='0'; d< '9'; d++)
+     {
+	     for(p=d+1; p<='9'; q++)
+	     {
+		     for(q=p+1;q <= '9' ; p ++)
+		     {
+			     if((p !=d) && P=='8')
+			     {  continue;
+
+			     putchar(',');
+			     putchar(',');
+			     }
+		     }
+	     }
+     }
+}
+
+
+
+
+
+
+
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,12 @@
+#holbertonschool-low_level_programming
+
+1.0-Positive_or_negative.c
+2.1-last_digit.
+3.2-print_alphabet.c
+4.    3-print_alphabets.c
+5.   #4-print_alphabt.c
+6.   #5-print_numbers.c
+7.   #6-print-numberz.c
+8.   #7-print_tebahpla.c
+9.   #8-print_base16.c
+10.  #9-print_comb.c
