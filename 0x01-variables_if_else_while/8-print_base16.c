2ca8dc0320a56db2fa68c26956026a62cf2107ff
MacindiCindi12 <sekhwarimasindi21@gmail.com>
2022-02-04 01:19:47 -0800

    Minor correction

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+
+ int main (void)
+ {
+     int d;
+     char low;
+
+     for(d='0' ; low <= 'f' ;low++)
+     putchar(d);
+
+     for(low ='a' low <='f' low++)
+     putchar(low);
+
+     putchar('\n');
+
+return (0);
+
+
+ }
