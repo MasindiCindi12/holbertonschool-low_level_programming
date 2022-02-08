f770f7c49d1895dd5501a8733e394cc3edfa4027
Masindi Cindy <59694595+MasindiCindi12@users.noreply.github.com>
2022-02-08 15:01:32 +0200

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_to_98 - a function that prints all natural numbers from  to 98
+ * print_to_98 - a function that prints all natural numbers from n to 98
  *
  * Return: Always 0 (Success)
  */
