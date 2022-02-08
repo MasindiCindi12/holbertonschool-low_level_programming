30d61c07843e538d933be994bf13c5b11d81b5a9
MacindiCindi12 <sekhwarimasindi21@gmail.com>
2022-02-07 22:44:00 -0800

    My First Commit

---
+++ 0x02-functions-nested-loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <holberton.h>
+/**
+ *
+ * Main - Entry _putchar : Writes the character c to stdou
+ *
+ * Return : On Success (1)
+ *
+ * 
+ */
+
+int main (void)
+{
+     char *sh="Holberton";
+
+     while (*sh)
+     {
+	     _putchar(*sh);
+	     sh++;
+     }
+
+      return (0);
+}
