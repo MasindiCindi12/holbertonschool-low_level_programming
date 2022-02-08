6148756a77ca171062a92d14a8bde151671c9912
Masindi Cindy <59694595+MasindiCindi12@users.noreply.github.com>
2022-02-08 14:23:41 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,6 @@
-#ifndef _HOLBERTON_h_
-#define _HOLBERTON_H_
-
-/**
- *
- * _putchar - writes the character c to stdout
- *
- * @c : The character to print
- *
- * Return : on success .
- *
- *
- */
 int _putchar (char c);
 void print_alphabet(void);
-void print_alphabet_x10(voi);
+void print_alphabet_x10(void);
 void isalpha(int c);
 void print_sign(int n);
 int _abs(int);
@@ -23,5 +10,3 @@ int times_table(void);
 int add(int a, int b);
 int print_to_98(int n);
 void print_tomes_table(int n);
-
-$endif
