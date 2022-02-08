c945b5cd286eb6584bd45dd712216f00181af5d2
Masindi Cindy <59694595+MasindiCindi12@users.noreply.github.com>
2022-02-08 15:28:14 +0200

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
-/*
- *
- *  is_lower - a function that checks for locase characters
- *
- *  Return: 1 Always
- *
- */
+/**
+  * _islower - Checks for lowercase character
+  * @c: The character to be checked
+  *
+  * Return: 1 for lowercase character or 0 for anything else
+  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
