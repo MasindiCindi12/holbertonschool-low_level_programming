f0da1afb04553b5b09f0471e95408e61475a95e5
MacindiCindi12 <sekhwarimasindi21@gmail.com>
2022-02-08 02:58:26 -0800

    corrections

---
+++ 0x02-functions_nested-loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ *
+ *  is_lower - a function that checks for locase characters
+ *
+ *  Return: 1 Always
+ *
+ */
+int _islower(int c)
+{
+	if(c >='a' && c <='z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x02-functions_nested-loops/main.c
@@ -0,0 +1,27 @@
+#ifndef _HOLBERTON_h_
+#define _HOLBERTON_H_
+
+/**
+ *
+ * _putchar - writes the character c to stdout
+ *
+ * @c : The character to print
+ *
+ * Return : on success .
+ *
+ *
+ */
+int _putchar (char c);
+void print_alphabet(void);
+void print_alphabet_x10(voi);
+void isalpha(int c);
+void print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int jack_bauer(int);
+int times_table(void);
+int add(int a, int b);
+int print_to_98(int n);
+void print_tomes_table(int n);
+
+$endif
