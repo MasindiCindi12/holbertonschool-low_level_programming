c062d0e366f112edbd5ba1e51d9e26eb92798560
MacindiCindi12 <sekhwarimasindi21@gmail.com>
2022-02-08 00:06:40 -0800

    Mandatory tasks

---
+++ 0x02-functions-nested-loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *
+ * _abs - a function that computes the absolute the value of an integer
+ *
+ * Return : asolute value of ab
+ *
+ */
+
+int _abs(int ab)
+{
+	return (ab * ((ab >0 ) -(ab < 0)));
+}
+
