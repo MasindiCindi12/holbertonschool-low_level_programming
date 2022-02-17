94c177367f76ee5d77bb62abb86d563047e08ea0
Masindi Cindy <59694595+MasindiCindi12@users.noreply.github.com>
2022-02-17 08:41:38 +0200

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-  * string_toupper - Changes all lowercase letters to uppercase
-  * @p: The string will be modified
-  *
-  * Return: char var
-  */
+ * string_toupper - Changes all lowercase letters to uppercase
+ * @p: The string will be modified
+ *
+ * Return: char var
+ */
 char *string_toupper(char *p)
 {
 	int a = 0;
