6f04ca93153ece7faf9c3ebe2e95593b9e9bc51e
Masindi Cindy <59694595+MasindiCindi12@users.noreply.github.com>
2022-02-17 08:40:52 +0200

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * string_toupper - Changes all lowercase letters to uppercase
- * @p: The string will be modified
- *
- * Return: char var
- */
+  * string_toupper - Changes all lowercase letters to uppercase
+  * @p: The string will be modified
+  *
+  * Return: char var
+  */
 char *string_toupper(char *p)
 {
 	int a = 0;
@@ -14,7 +14,7 @@ char *string_toupper(char *p)
 	{
 		if (p[a] >= 97 && p[a] <= 122)
 		{
-			p[a] -=32;
+			p[a] -= 32;
 		}
 
 		a++;
