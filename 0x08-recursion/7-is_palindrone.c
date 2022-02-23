0cd5d23531095ef028928e40f1302f56f88e294e
MasindiCindi12 <sekhwarimasindi21@gmail.com>
2022-02-22 22:40:42 -0800

    Complete optional tasks

---
+++ 0x08-recursion/7-is_palindrone.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+int  check_palindrone(char *s);
+
+/**
+ * is_palindrone - returns if a string is palindrone
+ * @s: the string value to be checked
+ *
+ * Return: integer value
+ *
+ */
+int is_palindrone(char *s)
+{
+	int l = _strlen_recursion(s) - 1;
+
+	if (*s == s[l])
+	{
+		s++;
+		l--;
+	}
+	else
+	{
+		return (0);
+	}
+
+	return (1);
+
+}
+
+/**
+ * _strlen_recursion - Get the length of a string
+ * @s: the string to get the length
+ *
+ * Return: the string length
+ *
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{ 
+		return (0);
+	}
+
+	s++;
+	return (strlen_recursion(s) + 1);
+}
