edf4a3f4cddcb6014cf5d6e5507bc50b0dbad5ed
Masindi Cindy <59694595+MasindiCindi12@users.noreply.github.com>
2022-02-23 08:52:03 +0200

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_strlen_reursion - Calculate the length of a string
+ *_strlen_recursion - Calculate the length of a string
  *@s: the string to count
  *
  * Return: integers value
