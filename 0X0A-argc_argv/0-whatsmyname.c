a665693203e00e2b15e10a65280b776370ac7827
MasindiCindi12 <sekhwarimasindi21@gmail.com>
2022-02-25 05:22:57 -0800

    mandatory tasks

---
+++ 0X0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Print its name
+ * @argc: argument count
+ * @ergv: argument vector
+ *
+ * Return: Alwayss zero (0)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s/n", argv[0];
+
+        return (0);
+}
