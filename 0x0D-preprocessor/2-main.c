84c0e7df9695ba63c4f685cc5567462fff211d6c
Masindi Cindy <59694595+MasindiCindi12@users.noreply.github.com>
2022-03-07 11:25:31 +0200

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print the files name of the program
+ *
+ * Return: Always zero
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (O);
+}
---
+++ 0x0D-preprocessor/READEME.md
@@ -0,0 +1,8 @@
+#0-object_like_macro.h
+#1-pi.h
+
+#2-main.c
+#4-sum.h
+
+#3-function_like_macro.h
+#101-preprocessor_abuse.c
